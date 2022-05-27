e4168e5ab6d6228a89e49d4b5c0c150b160245a2
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2022-05-27 19:54:39 +0200

    hw-02/08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+
+var throttle = require('lodash.throttle');
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+player.on(
+  'timeupdate',
+   _.throttle(function (data) {
+    console.log(data.seconds);
+    localStorage.setItem('videoplayer-current-time', `${data.seconds}`);
+  }, 1000),
+);
+
+let newTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(newTime);
+    
+
+
 
