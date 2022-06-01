a0c81b09e87ea119063446eb4cc4b34b64bc6bb1
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2022-06-01 09:41:19 +0200

    add correct

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,65 +2,37 @@ import throttle from 'lodash.throttle';
 
 const key = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
-updateOutput();
-form.addEventListener('input', throttle(saveRecords, 500));
+
+form.addEventListener('input', throttle(setRecords, 500));
 
 const email = document.querySelector('[name="email"]');
 const message = document.querySelector('[name="message"]');
 
-function saveRecords(evt) {
+function setRecords() {
   const record = {
     email: email.value,
     message: message.value,
   };
-  localStorage.setItem(key, JSON.stringify (record));
-  updateOutput();
-  form.reset();
+  
+  localStorage.setItem(key, JSON.stringify(record));
+
+  
 }
 
-function updateOutput() {
+function getRecords() {
   if (JSON.parse(localStorage.getItem(key) === null)) {
     return;
   } else {
-    record.email = JSON.parse(localStorage.getItem(key)).email || "";
-    record.message = Json.parse(localStorage.getItem(key)).message || "";
+    email.value = JSON.parse(localStorage.getItem(key)).email;
+    message.value = JSON.parse(localStorage.getItem(key)).message;
   }
 }
-updateOutput();
+getRecords();
 form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
-  console.log(load(key));
+  console.log(localStorage.getItem(key));
   localStorage.removeItem('key');
   form.reset();
 }
-/*function saveRecords () {
-    const records = {
-        email: email.value,
-        message: message.value,
-    };
-
-     localStorage.setItem('feedback-form-state', JSON.stringify(records))
-  
-}
-function getRecords() {
-  let object = JSON.parse(localStorage.getItem('feedback-form-state'));
-    if (object === null){
-        return;
-    } else {
-      email.value = object.email;
-      message.value = object.message; 
-    }
-}
-getRecords();
-
-form.addEventListener('submit', submitForm);
-
-function submitForm(evt) {
-    evt.preventDefault();
-    form.reset();
-    console.log(localStorage.getItem('feedback-form-state'));
-     localStorage.removeItem('feedback-form-state');
-}
-*/
